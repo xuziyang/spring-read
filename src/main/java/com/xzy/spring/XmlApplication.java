eae9f049ed6cd558d3c0bff25a81eac4925fd6b9
xuziyang <767637918@qq.com>
2022-03-15 11:00:30 +0800

    lifecycle

--- src/main/java/com/xzy/spring/XmlApplication.java
+++ src/main/java/com/xzy/spring/XmlApplication.java
@@ -1,7 +1,6 @@
 package com.xzy.spring;
 
-import com.xzy.spring.aop.Waiter;
-import com.xzy.spring.custom.xml.Student;
+import com.xzy.spring.lifecycle.LifeCycleBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,14 +8,17 @@ public class XmlApplication {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         
-        Student stu = (Student) context.getBean("stu01");
-        System.out.println(stu);
-    
-        Object boss = context.getBean("boss");
-        System.out.println(boss);
+//        Student stu = (Student) context.getBean("stu01");
+//        System.out.println(stu);
+//
+//        Object boss = context.getBean("boss");
+//        System.out.println(boss);
+//
+//        Waiter waiter = context.getBean(Waiter.class);
+//        waiter.greetTo("xzy");
 
-        Waiter waiter = context.getBean(Waiter.class);
-        waiter.greetTo("xzy");
+        LifeCycleBean lifeCycleBean = context.getBean("lifeCycleBean", LifeCycleBean.class);
+        System.out.println(lifeCycleBean.getName());
 
     }
 }
